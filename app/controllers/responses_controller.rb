02cb1bb7871afb1defd2472bd6ba9dbf8c750d56
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-03-19 18:00:10 +0530

    Edited responses controller with request.raw_post too

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,7 +1,10 @@
 class ResponsesController < ApplicationController
 	def index
+		binding.pry
 	end
 	def get_data
 		Response.create(:json_string => params)
+		Response.create(:json_string => request.raw_post)
+		redirect_to responses_path
 	end
 end

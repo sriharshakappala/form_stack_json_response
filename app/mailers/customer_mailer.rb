e259361605790a57b645f87212c5e17ac0623b12
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2015-03-25 13:15:41 +0530

    Reverting back :(

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -2,7 +2,7 @@ class CustomerMailer < ApplicationMailer
 
 	default from: "from@example.com"
 	
-	def send_confirmation_mailer(user, id)
+	def send_confirmation_mailer(user)
 		mail(to: user, subject: 'Get stuff done instantly!')
 	end
 
